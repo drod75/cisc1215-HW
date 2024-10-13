b079592293e09db472dacc1a47392cdfac95d87c
drod75 <dr507498@gmail.com>
2024-10-13 13:19:36 -0400

    edits

--- hw4/fines.py
+++ hw4/fines.py
@@ -1,10 +1,23 @@
 def fine_calc(speed, speed_limit):
-    pass
+    fine = 0
+    license_suspended = False
+    if speed >= speed_limit + 10:
+        fine += 50
+    
+    if speed > speed_limit + 10:
+        extra_speed = speed - (speed_limit + 10)
+        fine += (extra_speed * 5)
 
+
+    if speed > 90 and speed <= 100:
+        fine += 200
+    if speed > 100:
+        license_suspended = True
+    return fine, license_suspended  
 speed = float(input('Speed: '))
 speed_limit = float(input('Speed Limit: '))
 fs = fine_calc(speed, speed_limit)
-print(f'Fine: ${fs[0]}')
+print(f'Fine: ${fs[0]:.2f}')
 if fs[1]:
     print('License is suspended')
 else:
---
+++ hw4/testing.py
@@ -0,0 +1,4 @@
+def itself():
+    itself()
+
+itself()
